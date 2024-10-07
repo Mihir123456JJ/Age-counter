288ca5eda5140e4540b9b065e366496a683bf5d0
Mihir123456JJ <kalothiamihir@gmail.com>
2024-10-07 21:27:34 +0530

    Age counter

---
+++ main.py
@@ -0,0 +1,18 @@
+age=int(input("Enter your age:"))
+if age>10:
+    print("Allowed")
+    if age%2==0:
+        print("Even")
+    else:
+        print("Odd")    
+else:
+    print("Not allowed")
+    if age%2==0:
+        print("Even")
+    else:
+        print("Odd")    
+print("Thank You")        
+
+            
+
+       
